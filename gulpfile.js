781662d307ace269eda7c008a2271322bfa8dd61
minraid <miniraid@gmail.com>
2017-01-12 23:17:52 +0200

    commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const HubRegistry = require('gulp-hub');
+
+const conf = require('./conf/gulp.conf');
+
+// Load some files into the registry
+const hub = new HubRegistry([conf.path.tasks('*.js')]);
+
+// Tell gulp to use the tasks just loaded
+gulp.registry(hub);
+
+gulp.task('app', gulp.series('clean', 'webpack', 'browsersync'));
+gulp.task('server', gulp.series('compile', 'nodemon'));
+gulp.task('default', gulp.series('app', 'server'));
